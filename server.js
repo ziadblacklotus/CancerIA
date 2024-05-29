99472aaa9eec283a7a1794f45c8df69c403e1b01
Ziad BLAGOGIE <92253364+ziadblacklotus@users.noreply.github.com>
2024-05-29 10:41:35 +0100

    basic off my IA

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const tf = require('@tensorflow/tfjs-node');
+
+let model;
+
+(async () => {
+  model = await tf.loadLayersModel('file://model.json');
+})();
+
+app.use(express.json());
+
+app.post('/predict', async (req, res) => {
+  const input = tf.tensor(req.body.input);
+  const prediction = model.predict(input);
+  prediction.array().then(array => {
+    res.json(array);
+  });
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
