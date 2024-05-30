ca89fc82cb70d34e0811aa24ced2fd525cfda033
ziad <blagogiez@gmail.com>
2024-05-30 13:42:28 +0200

    add vercel.json config, modif package.json move model.json to /model

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const app = express();
 const tf = require('@tensorflow/tfjs-node');
+const path = require('path');
 
 let model;
 
+// Charger le modèle
 (async () => {
-  model = await tf.loadLayersModel('file://model.json');
+  model = await tf.loadLayersModel('file://model/model.json');
 })();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Route pour les prédictions
 app.post('/predict', async (req, res) => {
-  // window.print("prediction");
-  console.log("request prediction ...\n");
-  const input = tf.tensor(req.body.input);
-  const prediction = model.predict(input);
-  prediction.array().then(array => {
-    res.json(array);
-  });
+  try {
+    const input = tf.tensor(req.body.input);
+    const prediction = model.predict(input);
+    prediction.array().then(array => {
+      res.json(array);
+    });
+  } catch (error) {
+    console.error('Erreur lors de la prédiction:', error);
+    res.status(500).send('Erreur lors de la prédiction');
+  }
 });
 
-app.post('/test', (req, res) => {
-  console.log('testttttttt\n');
-})
-
+// Route pour servir le fichier index.html
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
